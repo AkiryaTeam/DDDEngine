4cc8ba6266438cc424644797e4fdfa6808ff0ff0
Kira Tanaka <suein_i@mail.ru>
2016-03-07 23:45:03 +0500

    Add line class

--- DDDEngine/Line.cs
+++ DDDEngine/Line.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace DDDEngine
 {
@@ -9,8 +11,38 @@ namespace DDDEngine
 
         public void Draw(Canvas canvas, Camera camera) // TODO: implement bresenham
         {
-            Start.Draw(canvas, camera);
-            End.Draw(canvas, camera);
+            List<Point> points = LineToWpfElement();
+            foreach (var point in points)
+            {
+                point.Draw(canvas, camera);
+            }
+        }
+        private List<Point> LineToWpfElement()
+        {
+            List<Point> points = new List<Point>();
+            int deltaX = Math.Abs(End.X - Start.X);
+            int deltaY = Math.Abs(End.Y - Start.Y);
+            int signX = Start.X < End.X ? 1 : -1;
+            int signY = Start.Y < End.Y ? 1 : -1;
+            int error = deltaX - deltaY;
+
+            while (Start.X != End.X || Start.Y != End.Y)
+            {
+                points.Add(new Point { X = Start.X, Y = Start.Y });
+                int error2 = error * 2;
+                if (error2 > -deltaY)
+                {
+                    error -= deltaY;
+                    Start.X += signX;
+                }
+                if (error2 < deltaX)
+                {
+                    error += deltaX;
+                    Start.Y += signY;
+                }
+            }
+            points.Add(End);
+            return points;
         }
     }
 }
